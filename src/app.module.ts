1248754967e0b17939f24ee3f01495a2d18bbff1
lehuygiang28 <lehuygiang28@gmail.com>
2024-03-29 10:10:54 +0700

    refactor(main): remove unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '~/logger';
 import { AppConfigModule } from '~/common/config';
 import { RedisModule } from '~/common/redis';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users';
 import { AuthModule } from './modules/auth';
 import { AuthGoogleModule } from './modules/auth-google';
@@ -33,7 +31,5 @@ import { OrdersModule } from './modules/orders/orders.module';
         CategoriesModule,
         OrdersModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
