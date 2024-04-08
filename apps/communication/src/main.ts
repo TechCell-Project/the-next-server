301fc3e9d8ee0ff0af275c9b70aa9a7417c8c516
lehuygiang28 <lehuygiang28@gmail.com>
2024-04-08 17:54:28 +0700

    Update logger configuration in main.ts and add pino-pretty package to package.json

--- apps/communication/src/main.ts
+++ apps/communication/src/main.ts
@@ -4,11 +4,12 @@ import { Logger } from 'nestjs-pino';
 import { RabbitMQService } from '~/common/rabbitmq';
 
 async function bootstrap() {
-    const app = await NestFactory.create(CommunicationModule);
-
+    const app = await NestFactory.create(CommunicationModule, { bufferLogs: true });
     const port = process.env.COMMUNICATION_PORT || 8001;
 
     const logger = app.get(Logger);
+    app.useLogger(logger);
+
     RabbitMQService.connectRabbitMQ({
         app,
         queueNameEnv: 'COMMUNICATION_QUEUE',
