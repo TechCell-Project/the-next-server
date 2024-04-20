c4720d956fed6d0d13c4ed8c8b3f3615a9bebb24
lehuygiang28 <lehuygiang28@gmail.com>
2024-04-21 02:04:56 +0700

    feat(orders): :zap: auto calculate totalPrice of order

--- apps/server/src/modules/orders/dtos/preview-order-response.dto.ts
+++ apps/server/src/modules/orders/dtos/preview-order-response.dto.ts
@@ -2,8 +2,18 @@ import { OmitType } from '@nestjs/swagger';
 import { Order } from '../schemas';
 
 export class PreviewOrderResponseDto extends OmitType(Order, ['_id', 'note', 'orderStatus']) {
-    constructor(data: PreviewOrderResponseDto) {
+    constructor(data: Omit<PreviewOrderResponseDto, 'totalPrice'>) {
         super();
         Object.assign(this, data);
+
+        this.totalPrice =
+            0 +
+            this.shipping.fee +
+            this.products.reduce((total: number, product) => {
+                return (
+                    total +
+                    (product?.unitPrice?.special || product.unitPrice.base) * product.quantity
+                );
+            }, 0);
     }
 }
