e302bf5adca98dd9845d77a75fac6ccc8c522c50
lehuygiang28 <lehuygiang28@gmail.com>
2024-05-29 21:38:12 +0700

    feat: :zap: add options to disable logger, shorten request log
    [skip sdk]

--- libs/logger/src/logger.module.ts
+++ libs/logger/src/logger.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from '~/common/config/config.module';
 import { ConfigService } from '@nestjs/config';
 import { LoggerModule as PinoLogger } from 'nestjs-pino';
+import { isTrueSet } from '~/common';
+import { Request } from 'express';
 
 @Module({
     imports: [
@@ -12,16 +14,32 @@ import { LoggerModule as PinoLogger } from 'nestjs-pino';
                 /**
                  * Can config some async/promise options here
                  */
+                const isProduction = config.getOrThrow<string>('nodeEnv') === 'production';
+                const disableLogger = isTrueSet(config.get<boolean>('DISABLE_LOGGER', false));
+
+                if (disableLogger) {
+                    return {
+                        pinoHttp: {
+                            enabled: false,
+                        },
+                    };
+                }
+
                 return {
                     pinoHttp: {
-                        level:
-                            config.getOrThrow<string>('nodeEnv') !== 'production'
-                                ? 'debug'
-                                : 'info',
-                        transport:
-                            config.getOrThrow<string>('nodeEnv') !== 'production'
-                                ? { target: 'pino-pretty' }
-                                : undefined,
+                        level: isProduction ? 'info' : 'debug',
+                        transport: isProduction ? undefined : { target: 'pino-pretty' },
+                        ...(isProduction
+                            ? {}
+                            : {
+                                  serializers: {
+                                      req: (req: Request) => ({
+                                          id: req.id,
+                                          method: req.method,
+                                          url: req.url,
+                                      }),
+                                  },
+                              }),
                     },
                 };
             },
